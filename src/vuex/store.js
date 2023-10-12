8e7a2ac32dc9dbdf93e52062baa8b02ecda702d5
Andy Lelli <andylelli@yahoo.com>
2023-10-12 19:46:42 +0100

    commit

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -6,7 +6,7 @@ import { createStore } from 'vuex'
 
 export const store = createStore({
   state: {
-    version: "v8 20231012_02",
+    version: "v8 20231012_03",
 
     url: "https://www.evaria.io/public/",
     urlQR: "https://www.evaria.io/admin/index.html?",
