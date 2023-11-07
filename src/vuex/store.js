7804039b3b5de4f413b15d5eca92bf1b7d10c711
Andy Lelli <andylelli@yahoo.com>
2023-11-07 20:27:19 +0000

    commit

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -9,7 +9,7 @@ var url = "https://www.evaria.io/";
 
 export const store = createStore({
   state: {
-    version: "v8 20231104_01",
+    version: "v8 20231107_01",
     url: url + "public/",
     cache: url + "admin/",
     urlQR: url + "index.html?",
